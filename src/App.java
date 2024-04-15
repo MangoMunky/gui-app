2726b420cce7bdc019e900e3d1211374730adf45
MangoMunky <boczonszymon@gmail.com>
2024-04-15 09:53:38 -0400

    initial commit yo

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String name = JOptionPane.showInputDialog
+        ("Enter your name");
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+
+        JOptionPane.showMessageDialog(null, "Youre name is " + name);
+        JOptionPane.showMessageDialog(null, "You are " + age + " years old");
+    }
+}
